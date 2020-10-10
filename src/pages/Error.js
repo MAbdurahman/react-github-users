c38c9f7c69d698450a1b3a24307b02e7f0a7ac4f
MAbdurahman <mdbdrrhm@gmail.com>
2020-10-10 08:25:35 -0500

    Complete Error.js

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+
 const Error = () => {
-  return <h2>error page</h2>;
+	return (
+		<Wrapper>
+			<div>
+				<h1>404</h1>
+				<h3>page cannot be found</h3>
+				<Link to='/' className='btn'>
+					back to home
+				</Link>
+			</div>
+		</Wrapper>
+	);
 };
+
 const Wrapper = styled.section`
-  min-height: 100vh;
-  display: grid;
-  place-items: center;
-  background: var(--clr-primary-10);
-  text-align: center;
-  h1 {
-    font-size: 10rem;
-  }
-  h3 {
-    color: var(--clr-grey-3);
-    margin-bottom: 1.5rem;
-  }
+	min-height: 100vh;
+	display: grid;
+	place-items: center;
+	background: var(--clr-primary-10);
+	text-align: center;
+	h1 {
+		font-size: 10rem;
+	}
+	h3 {
+		color: var(--clr-grey-3);
+		margin-bottom: 1.5rem;
+		font-size: 2rem;
+	}
 `;
 export default Error;
